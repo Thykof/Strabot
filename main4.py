6b1e710bf305c6c9acfc2930ee9e3727357c3f2b
Nathan Seva <thykof@protonmail.ch>
2020-04-04 21:46:51 +0200

    Compare 4 algos

--- main4.py
+++ main4.py
@@ -1,4 +1,4 @@
-from sklearn import tree, svm
+from sklearn import tree, svm, gaussian_process
 from sklearn.model_selection import train_test_split
 from sklearn import linear_model
 from sklearn import preprocessing
@@ -13,48 +13,67 @@ def raw_data_to_numeric(raw_data):
     data = data.replace(',', '.')
     return float(data)
 
-def train_model(df, algo, n):
+def split_df(df, test_size=0.2):
+    n = df.shape[1]
     x = df.iloc[:,:n-1]
     y = df.iloc[:,n-1]
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+    return train_test_split(x, y, test_size=test_size)
+
+
+def train_model(df, algo, n):
+    x_train, x_test, y_train, y_test = split_df(df)
     model = algo.fit(x_train, y_train)
     return model, model.score(x_test, y_test)
 
-def simulate(df, model, m=100, threshold=100):
+
+
+def evaluate(df, model, m=100, threshold=100):
     """
-    Iterate over m first rows and simulate trades.
+    Iterate over m first rows and evaluate trades.
     Trades only if price move more than the threshold.
 
     """
     df = df.loc[:m]
-    prix_achats = list()
-    prix_ventes = list()
-    diffs = list()
+    buy_prices = list()
+    sell_prices = list()
+    tot_error = int()
     for index, row in df.iterrows():
         x = row.head(df.shape[1]-1)
+        y = row.tail(1).to_list()[0]
+        x_decision = row.tail(2).to_list()[0]
+
         prediction = model.predict([x])[0]
-        x_decision = row.tail(1).to_list()[0]
-        # print(str(prediction) + " " + str(x_decision))
-        if abs(prediction - x_decision) > threshold:
+        error = abs(prediction - y)
+        # evaluate trade
+        if abs(x_decision - prediction) > threshold:
             if prediction > x_decision:
                 # buy
-                prix_achats.append(x_decision)
-                prix_ventes.append(prediction)
+                buy_prices.append(x_decision)
+                sell_prices.append(prediction)
             else:
-                prix_achats.append(prediction)
-                prix_ventes.append(x_decision)
-    if len(prix_achats) and len(prix_ventes):
-        prix_achat_moy = statistics.mean(prix_achats)
-        prix_vente_moy = statistics.mean(prix_ventes)
+                buy_prices.append(prediction)
+                sell_prices.append(x_decision)
+
+        # calculate error
+        tot_error += error
+
+    # end of for loop
+    if len(buy_prices) and len(sell_prices):
+        prix_achat_moy = statistics.mean(buy_prices)
+        prix_vente_moy = statistics.mean(sell_prices)
         # print(prix_achat_moy)
         # print(prix_vente_moy)
-        return (((prix_vente_moy / prix_achat_moy) - 1) * 100)
+    else:
+        prix_achat_moy = 0
+        prix_vente_moy = 0
+    return (((prix_vente_moy / prix_achat_moy) - 1) * 100), tot_error / m
 
 def main(algo, n=14, k=50):
     """
     Create row of n days.
     Train and test k models.
     """
+    print(str(algo)[:str(algo).find('(')])
     df = pandas.read_csv("bitcoin-price-all.csv", sep=';')
     # df = df.loc[:99]
     df = df[['Close**']]
@@ -75,19 +94,44 @@ def main(algo, n=14, k=50):
 
     tot_pnl = int() # total profit and loss
     tot_score = int()
+    tot_error = int()
     # train several models
     for i in range(k):
         model, score = train_model(new_df, algo, n)
-        tot_pnl += simulate(new_df, model, n)
         tot_score += score
-    print(tot_pnl / k) # 5.262569795325347
-    print(tot_score / k) # 0.9835991372642395
-
+        pnl, error = evaluate(new_df, model, n)
+        tot_pnl += pnl
+        tot_error = error
+    print('pnl:   ' + str(tot_pnl / k))
+    print('score: ' + str(tot_score / k))
+    print('error: ' + str(tot_error / k))
+    print()
 
 if __name__ == '__main__':
-    main(svm.SVR(), 14, 100)
-    main(linear_model.LinearRegression(), 14, 100)
-    # 899.5609988944342
-    # -0.4233642146716479
-    # 5.434163478138874
-    # 0.9869320833349131
+    n = 15
+    k = 100
+    main(svm.SVR(), n, k)
+    main(linear_model.SGDRegressor(), n, k)
+    main(linear_model.LinearRegression(), n, k)
+    main(gaussian_process.GaussianProcessRegressor(), n, k)
+    """
+    SVR
+    pnl:   920.0377622338833
+    score: -0.40757203601020864
+    error: 84.06736743459896
+
+    SGDRegressor
+    pnl:   3326314273912839.0
+    score: -8.365597114192531e+27
+    error: 265865377123154.0
+
+    LinearRegression
+    pnl:   3.9391180976549163
+    score: 0.981791386758865
+    error: 3.493815099979772
+
+    GaussianProcessRegressor
+    pnl:   62.6189457666077
+    score: -0.7321396045392251
+    error: 17.448546674346023
+    """
