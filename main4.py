0971324a4f8b96abf3cdcca13ad0c7d79ff53cf2
Nathan Seva <thykof@protonmail.ch>
2020-04-05 19:42:38 +0200

    Improve evaluation

--- main4.py
+++ main4.py
@@ -19,25 +19,17 @@ def split_df(df, test_size=0.2):
     y = df.iloc[:,n-1]
     return train_test_split(x, y, test_size=test_size)
 
-def train_model(df, algo):
-    x_train, x_test, y_train, y_test = split_df(df)
-    model = algo.fit(x_train, y_train)
-    return model, model.score(x_test, y_test)
-
-def evaluate(df, model, m=100, threshold=50):
-    """
-    Iterate over m first rows and evaluate trades.
-    Trades only if price move more than the threshold.
-
-    """
-    df = df.loc[:m]
+def evaluate(df_tuple, model, threshold):
+    """Trades only if price move more than the threshold."""
+    x_test, y_test = df_tuple
     buy_prices = list()
     sell_prices = list()
     tot_error = int()
-    for index, row in df.iterrows():
-        x = row.head(df.shape[1]-1)
-        y = row.tail(1).to_list()[0]
-        x_decision = row.tail(2).to_list()[0]
+    n = df_tuple[0].shape[0]
+    for i in range(n):
+        x = x_test.iloc[i]
+        y = y_test.iloc[i]
+        x_decision = x.iloc[-1]
 
         prediction = model.predict([x])[0]
         error = abs(prediction - y)
@@ -61,13 +53,13 @@ def evaluate(df, model, m=100, threshold=50):
     if len(buy_prices) and len(sell_prices):
         average_buy_price = statistics.mean(buy_prices)
         average_sell_price = statistics.mean(sell_prices)
-        return (((average_sell_price / average_buy_price) - 1) * 100), tot_error / m
+        return (((average_sell_price / average_buy_price) - 1) * 100), tot_error / n
     else:
         average_buy_price = 0
         average_sell_price = 0
-        return 0, tot_error / m
+        return 0, tot_error / n
 
-def train_algo(algo, k, n):
+def train_algo(algo, k, n, threshold):
     """
     Create row of n days.
     Train and test k models.
@@ -93,16 +85,17 @@ def train_algo(algo, k, n):
                 row = list()
                 i = 0
     new_df = pandas.DataFrame(new_df_list)
-    m = new_df.size # evaluate with the m first rows
 
     tot_pnl = int() # total profit and loss
     tot_score = int()
     tot_error = int()
     # train several models
     for i in range(k):
-        model, score = train_model(new_df, algo)
-        tot_score += score
-        pnl, error = evaluate(new_df, model, m)
+        x_train, x_test, y_train, y_test = split_df(new_df)
+        model = algo.fit(x_train, y_train)
+        tot_score += model.score(x_test, y_test)
+        # pnl, error = evaluate(new_df, model)
+        pnl, error = evaluate((x_test, y_test), model, threshold)
         tot_pnl += pnl
         tot_error = error
     pnl = tot_pnl / k
@@ -116,18 +109,20 @@ def train_algo(algo, k, n):
     #     print()
     return algo_name, pnl, score, error
 
-def main(algos, k_range=(10, 40), n_range=(4, 100)):
+def main(algos, k_range=(10, 40), n_range=(4, 100), threshold=50):
     tries = list()
     for algo in algos:
         for k in range(k_range[0], k_range[1]):
             for n in range(n_range[0], n_range[1]):
-                algo_name, pnl, score, error = train_algo(algo, k, n)
+                algo_name, pnl, score, error = train_algo(algo, k, n, threshold)
                 tries.append(((algo, k, n), (algo_name, pnl, score, error)))
 
-    max_pnl = int()
+    max_pnl = 0
     max_pnl_config = None
     min_error = math.inf
     min_error_config = None
+    max_score = 0
+    max_score_config = None
     for result in tries:
         if DEBUG:
             print(result)
@@ -137,7 +132,10 @@ def main(algos, k_range=(10, 40), n_range=(4, 100)):
         if min_error > result[1][3]:
             min_error = result[1][3]
             min_error_config = result
-    return min_error_config, max_pnl_config
+        if max_score < result[1][2]:
+            max_score = result[1][2]
+            max_score_config = result
+    return min_error_config, max_pnl_config, max_score_config
 
 if __name__ == '__main__':
     b = time.time()
@@ -145,14 +143,17 @@ if __name__ == '__main__':
     # n: number of days in each row
     algos = list()
     algos.append(linear_model.LinearRegression())
-    k_ = 200
-    min_error_config, max_pnl_config = main(algos, k_range=(k_, k_+1), n_range=(237, 273))
+    k_ = 50
+    threshold = 20
+    min_error_config, max_pnl_config, max_score_config = main(algos, k_range=(k_, k_+1), n_range=(5, 25), threshold=threshold)
     print()
     print(min_error_config)
     print(max_pnl_config)
+    print(max_score_config)
     print(time.time() - b)
     """
-((LinearRegression(copy_X=True, fit_intercept=True, n_jobs=None, normalize=False), 200, 250), ('LinearRegression', 54.169102067963614, -26.177404755210638, 0.00021548993044429528))
-((LinearRegression(copy_X=True, fit_intercept=True, n_jobs=None, normalize=False), 200, 267), ('LinearRegression', 811.2526366303829, -3.079701960845952, 0.01576299268038821))
-54.55777025222778
+((LinearRegression(copy_X=True, fit_intercept=True, n_jobs=None, normalize=False), 50, 24), ('LinearRegression', 72.89835528418281, 0.9864903910464595, 1.431114771854653))
+((LinearRegression(copy_X=True, fit_intercept=True, n_jobs=None, normalize=False), 50, 8), ('LinearRegression', 170.68796262633748, 0.9897190268544935, 3.7451735321168513))
+((LinearRegression(copy_X=True, fit_intercept=True, n_jobs=None, normalize=False), 50, 9), ('LinearRegression', 29.91841178805823, 0.9953372053890338, 2.282855057393496))
+13.589125871658325
     """
